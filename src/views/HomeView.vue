beda4beb004159f4bfcfc8462f24e60959b90f1f
FERZ <arFReZa@yandex.ru>
2023-06-27 12:51:08 +0300

    d

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <button @click="get_name">Close</button>
+    <p>{{nickname}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+  import { onMounted } from 'vue'
+  const tg = window.Telegram.WebApp;
+  onMounted(() => {
+    this.nickname = tg.initDataUnsafe.user.username;
+    console.log(`the component is now mounted.`)
+  })
 export default {
   name: 'HomeView',
   data(){
     return{
-      smth:0
+      nickname:""
     }
   },
+
   methods:{
     get_name(){
-      const tg = window.Telegram.WebApp;
+      
       tg.close();
     }
   }
