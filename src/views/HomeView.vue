380a55fdb54771ae1f3f91e56199854edae6492d
FERZ <arFReZa@yandex.ru>
2023-06-29 21:19:12 +0300

    sd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,30 +2,30 @@
   <div class="home" v-if="loaded">
     <img alt="Vue logo" src="../assets/logo.png">
     <button @click="get_name">Close</button>
-    <p>Nick {{nickname}}</p>
+    <!-- <p>Nick {{nickname}}</p>
     <p>Params: {{ params }}</p>
     <div class = "offers">
       <div class="offer" v-for="offer in offers" :key="offer">
         <p>Cost: {{offer.cost }}</p>
       </div>
 
-    </div>
+    </div> -->
   </div>
 </template>
  
 <script>
 // @ is an alias to /src
-  import serv from "../api/serv.js"
+  // import serv from "../api/serv.js"
   const tg = window.Telegram.WebApp;
 export default {
   name: 'HomeView',
   async mounted(){
     this.loaded = true;
-    this.params = this.$route.query;
+    // this.params = this.$route.query;
 
-    this.nickname = tg?.initDataUnsafe?.user?.username;
-    let resp = await serv.params(this.params);
-    this.offers = resp.data;
+    // this.nickname = tg?.initDataUnsafe?.user?.username;
+    // let resp = await serv.params(this.params);
+    // this.offers = resp.data;
 
     
 
