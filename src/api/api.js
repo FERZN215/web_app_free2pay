2c70c5ed7cdb475a7c886930da8c6c65df313d8a
FERZ <arFReZa@yandex.ru>
2023-07-13 14:03:09 +0300

    test

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default ()=>{
     return axios.create({
-        baseURL:"http://localhost:5001/"
+        baseURL:"http://http://62.217.179.179:5001/"
         // baseURL:'http://62.217.178.101:5000'
     })
 }
